2a492517cb534af3b2c25462fb9e36368d4e2473
dazklein <lupardima@list.ru>
2020-12-07 13:27:13 +0300

    Add forms

---
+++ CashTransactionsApp/EditForms/EditPositionForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashTransactionsApp.EditForms
+{
+    public partial class editPositionForm : Form
+    {
+        public editPositionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
